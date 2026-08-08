359ec93d4c22f87712f9fcd0794110436bb44da6
Ranjithkumar352 <kareranjith458@gmail.com>
2026-08-08 23:29:29 +0530

    Fix analytics API endpoint

--- src/services/analytics.service.ts
+++ src/services/analytics.service.ts
@@ -1,7 +1,11 @@
 import api from "@/lib/axios";
 
-export const getAnalytics = async () => {
-  const res = await api.get("/analytics");
+export const getAnalytics = async (
+  datasetId: string
+) => {
+  const res = await api.get(
+    `/analytics/${datasetId}`
+  );
 
   return res.data;
 };
